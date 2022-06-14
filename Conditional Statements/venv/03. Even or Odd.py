9b98d13c23a86e32249d50ca9c956df4d201f066
Vlado-Iliev <102687116+Vlado-Iliev@users.noreply.github.com>
2022-06-15 00:08:46 +0100

    reorganizing repo

--- Conditional Statements/venv/03. Even or Odd.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-
-even_numb = a % 2
-
-if even_numb == 0 :
-    print ("even")
-else :
-    print ("odd")
--- Conditional Statements/venv/05. Password Guess.py
+++
@@ -1,6 +0,0 @@
-a = input()
-
-if a == "s3cr3t!P@ssw0rd" :
-    print ("Welcome")
-else :
-    print ("Wrong password!")
